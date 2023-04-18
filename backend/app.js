da6ae7e88bd8e166a6d4d2d5509ff8c84ad1aa4c
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-04-18 13:06:07 +0200

    boilerplate

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:5173",
 
-app.use(logger('dev'));
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-module.exports = app;
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("hej", (arg) => {
+    console.log(arg);
+    io.emit("hej", arg + " Anton");
+    io.emit("hejhej", arg + " Anton");
+  });
+});
+
+module.exports = { app: app, server: server };
--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,24 @@
 import "./style.css";
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
+const socket = io("http://localhost:3000");
+
 document.querySelector("#app").innerHTML = `
-  <div class="flex justify-center items-center h-screen bg-teal-900">
-    <h1 class="bg-red-500 text-6xl font-extrabold p-4">Hello World</h1>
+  <div class="h-screen bg-teal-900 parent">
+    <header class="div1 w-full h-full">header</header>
+      <section class="div5 w-full h-full">regler</section>
+      <section class="div4 w-full h-full">sparade bilder</section>
+      <main class="div3 w-full h-full">spelet/lobbys</main>
+      <aside class="div2 w-full h-full">chat</aside>
   </div>
 `;
+
+socket.emit("hej", "Hej");
+
+socket.on("hej", (arg) => {
+  console.log(arg);
+});
+
+socket.on("hejhej", (arg) => {
+  console.log(arg);
+});
