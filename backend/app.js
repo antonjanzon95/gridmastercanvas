e7fd240c6fcd3255b164151aed4c3adb00581378
robin-sevelin <robinsevelin@hotmail.com>
2023-04-19 12:58:36 +0200

    added log form.js

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const imageRouter = require("./routes/image");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const imageRouter = require('./routes/image');
 // const { MongoClient } = require("mongodb");
 
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server, {
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
 
-    methods: ["GET", "POST"],
+    methods: ['GET', 'POST'],
   },
 });
 
 mongoose.connect(
-  "mongodb+srv://antonjanzon123:antonjanzon123@test-cluster.cq7iazz.mongodb.net/test",
+  'mongodb+srv://antonjanzon123:antonjanzon123@test-cluster.cq7iazz.mongodb.net/test',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -29,34 +29,52 @@ mongoose.connect(
 );
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Connected successfully');
 });
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/image", imageRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/image', imageRouter);
 
-// io.on("connection", (socket) => {
-//   console.log("user connected");
+io.on('connection', (socket) => {
+  socket.on('saveUser', (arg) => {
+    socket.userName = arg;
+    socket.userColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
+    let users = [];
 
-//   socket.on("hej", (arg) => {
-//     console.log(arg);
-//     io.emit("hej", arg + " Anton");
-//     io.emit("hejhej", arg + " Anton");
-//   });
-// });
+    let user = {
+      userName: socket.userName,
+      userId: socket.id,
+      userColor: socket.userColor,
+    };
+
+    users.push(user);
+    console.log(users);
+
+    io.emit('saveUser', { user });
+  });
+
+  socket.on('chat', (arg) => {
+    socket.userMessage = arg;
+
+    let chatMessage = {
+      userColor: socket.userColor,
+      userName: socket.userName,
+      userMessage: socket.userMessage,
+    };
+
+    io.emit('chat', { chatMessage });
+  });
+});
 
 module.exports = { app: app, server: server };
