d27f45b723b71c34ddb1c584a08986e0b84a7a26
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-04-23 21:14:09 +0200

    added highscore page render and fetch

--- frontend/scripts/gameover.js
+++ frontend/scripts/gameover.js
@@ -1,3 +1,4 @@
+import { createButton } from "./buttons";
 import { renderRoomsSection } from "./gameRooms";
 
 export const showGameOverPage = (finalScore) => {
@@ -15,15 +16,47 @@ export const showGameOverPage = (finalScore) => {
     renderRoomsSection();
   });
 
-  const highscoreBtn = document.createElement("button");
-  highscoreBtn.classList.add("btn");
-  highscoreBtn.innerHTML = "Highscores";
-  highscoreBtn.addEventListener("click", () => {});
+  const highScoreBtn = createButton("Highscores", showHighScorePage);
 
-  mainContainer.appendChild(highscoreBtn);
+  mainContainer.appendChild(highScoreBtn);
   mainContainer.appendChild(playAgainBtn);
   mainContainer.appendChild(score);
 };
+
+async function showHighScorePage() {
+  const mainContainer = document.querySelector("main");
+
+  const highScoresContainer = document.createElement("div");
+  highScoresContainer.classList.add("highscores-container");
+
+  const highScores = await fetchHighscores();
+
+  highScores.forEach((highscore) => {
+    const scoreContainer = document.createElement("div");
+    scoreContainer.classList.add("highscore-container");
+
+    const users = document.createElement("p");
+    users.innerHTML = highscore.users.join();
+
+    const score = document.createElement("p");
+    score.innerHTML = highscore.score;
+
+    const date = document.createElement("p");
+    date.innerHTML = highscore.date.toLocaleString();
+
+    scoreContainer.append(users, score, date);
+    highScoresContainer.appendChild(scoreContainer);
+  });
+
+  mainContainer.appendChild(highScoresContainer);
+}
+
+async function fetchHighscores() {
+  const response = await fetch("http://localhost:3000/highscores");
+  const data = await response.json();
+
+  return data;
+}
 
 // calculate score
 export function compareTryToSolution(currentGrid, solution) {
