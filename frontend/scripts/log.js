bd357285db47418c48c25293a770dbea05dad7ca
robin-sevelin <robinsevelin@hotmail.com>
2023-04-24 21:07:33 +0200

    render site nav when logged in

--- frontend/scripts/log.js
+++ frontend/scripts/log.js
@@ -3,27 +3,34 @@ import { renderChatHtml } from './chatcomp';
 import { renderRoomsSection } from './gameRooms';
 
 export function initLog() {
+  renderLogo();
   if (sessionStorage.getItem('user')) {
     console.log('logged in');
-    renderLogoutButton();
     renderChatHtml();
     renderRoomsSection();
+    renderSiteNav();
+    renderLogoutButton();
   } else {
     socket.off('saveUser');
-
     console.log('not logged in');
-    const chatDiv = document.querySelector('#chat-div');
-    const mainContainer = document.querySelector('main');
-    mainContainer.innerHTML = 'logga in för att använda gridmaster';
-    chatDiv.innerHTML = '';
+    renderWelcome();
     renderLogForm();
   }
+}
+
+function renderWelcome() {
+  let div = document.createElement('div');
+  let header = document.querySelector('header');
+
+  div.innerHTML =
+    'Välkommen till gridmaster Canvas, logga in för att börja måla';
+
+  header.append(div);
 }
 
 function renderLogForm() {
   let header = document.querySelector('header');
   let logForm = document.createElement('div');
-
   let logInput = document.createElement('input');
   let logUserButton = document.createElement('button');
 
@@ -47,9 +54,10 @@ function renderLogForm() {
 
     socket.emit('saveUser', JSON.parse(sessionStorage.getItem('user')));
 
-    initLog();
+    header.innerHTML = '';
     logInput.value = '';
     logForm.innerHTML = '';
+    initLog();
   });
 
   socket.on('userLoggedIn', (data) => {
@@ -64,7 +72,37 @@ function renderLogForm() {
   });
 }
 
+function renderLogo() {
+  let header = document.querySelector('#header');
+  let div = document.createElement('div');
+  let h1 = document.createElement('h1');
+
+  h1.innerHTML = 'Gridmaster';
+
+  header.appendChild(div);
+  div.appendChild(h1);
+}
+
+function renderSiteNav() {
+  let header = document.querySelector('header');
+  let navContainer = document.createElement('div');
+
+  navContainer.classList.add('navContainer');
+
+  let navImg = document.createElement('button');
+  let navPlay = document.createElement('button');
+  let navScore = document.createElement('button');
+
+  navImg.innerHTML = 'Saved images';
+  navPlay.innerHTML = 'How to play';
+  navScore.innerHTML = 'Highscores';
+
+  header.appendChild(navContainer);
+  navContainer.append(navImg, navPlay, navScore);
+}
+
 function renderLogoutButton() {
+  let chatDiv = document.querySelector('#chat-div');
   let mainContainer = document.querySelector('main');
   let header = document.querySelector('header');
   let logForm = document.createElement('div');
@@ -79,8 +117,10 @@ function renderLogoutButton() {
     sessionStorage.removeItem('user');
     sessionStorage.removeItem('color');
 
-    logForm.innerHTML = '';
     mainContainer.innerHTML = '';
+
+    chatDiv.innerHTML = '';
+    header.innerHTML = '';
     initLog();
   });
 }
