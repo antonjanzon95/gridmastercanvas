9674fbfc07c7a704fa8f4f5100bf67d752c964de
robin-sevelin <robinsevelin@hotmail.com>
2023-04-24 19:40:54 +0200

    disable main content when logged out

--- frontend/scripts/log.js
+++ frontend/scripts/log.js
@@ -1,43 +1,47 @@
 import { socket } from '../main';
 import { renderChatHtml } from './chatcomp';
+import { renderRoomsSection } from './gameRooms';
 
 export function initLog() {
   if (sessionStorage.getItem('user')) {
     console.log('logged in');
     renderLogoutButton();
     renderChatHtml();
+    renderRoomsSection();
   } else {
     socket.off('saveUser');
 
     console.log('not logged in');
-    const chatDiv = document.querySelector("#chat-div");
+    const chatDiv = document.querySelector('#chat-div');
+    const mainContainer = document.querySelector('main');
+    mainContainer.innerHTML = 'logga in för att använda gridmaster';
     chatDiv.innerHTML = '';
     renderLogForm();
   }
 }
 
 function renderLogForm() {
-  let header = document.querySelector("header");
-  let logForm = document.createElement("div");
+  let header = document.querySelector('header');
+  let logForm = document.createElement('div');
 
-  let logInput = document.createElement("input");
-  let logUserButton = document.createElement("button");
+  let logInput = document.createElement('input');
+  let logUserButton = document.createElement('button');
 
-  logInput.type = "text";
-  logInput.placeholder = "name";
-  logUserButton.innerHTML = "log in";
+  logInput.type = 'text';
+  logInput.placeholder = 'name';
+  logUserButton.innerHTML = 'log in';
 
   header.appendChild(logForm);
   logForm.append(logInput, logUserButton);
 
-  logUserButton.addEventListener("click", (e) => {
+  logUserButton.addEventListener('click', (e) => {
     e.preventDefault();
 
-    if (logInput.value === "") {
+    if (logInput.value === '') {
       return;
     }
 
-    let user = {name: logInput.value, color: '', id: ''};
+    let user = { name: logInput.value, color: '', id: '' };
 
     sessionStorage.setItem('user', JSON.stringify(user));
 
@@ -47,26 +51,26 @@ function renderLogForm() {
     logInput.value = '';
     logForm.innerHTML = '';
   });
-  
+
   socket.on('userLoggedIn', (data) => {
     console.log(data);
     const user = data.user;
     sessionStorage.setItem('user', JSON.stringify(user));
-    
+
     console.log(`${user.name} has logged in with color ${user.color}`);
     const color = user.color;
     console.log(color);
-    sessionStorage.setItem('color', color)
-    
+    sessionStorage.setItem('color', color);
   });
 }
 
 function renderLogoutButton() {
+  let mainContainer = document.querySelector('main');
   let header = document.querySelector('header');
   let logForm = document.createElement('div');
   let logOutButton = document.createElement('button');
 
-  logOutButton.innerHTML = "log out";
+  logOutButton.innerHTML = 'log out';
   header.appendChild(logForm);
   logForm.appendChild(logOutButton);
 
@@ -75,7 +79,8 @@ function renderLogoutButton() {
     sessionStorage.removeItem('user');
     sessionStorage.removeItem('color');
 
+    logForm.innerHTML = '';
+    mainContainer.innerHTML = '';
     initLog();
-    logForm.innerHTML = "";
   });
 }
