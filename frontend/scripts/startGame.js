5df1385e73fad7291e3d4e8ea436922887d2dbec
robin-sevelin <robinsevelin@hotmail.com>
2023-04-24 15:09:44 +0200

    removed bug

--- frontend/scripts/startGame.js
+++ frontend/scripts/startGame.js
@@ -1,32 +1,32 @@
-import { socket } from "../main";
-import { renderRoomUsers } from "./gameLobby";
-import { saveScore, showGameOverPage } from "./gameover";
-import { createGrid } from "./paintGrid";
+import { socket } from '../main';
+import { renderRoomUsers } from './gameLobby';
+import { showGameOverPage } from './gameover';
+import { createGrid } from './paintGrid';
 
 export function createGamePage(room) {
-  socket.off("readyCheck");
+  socket.off('readyCheck');
 
-  const mainContainer = document.querySelector("main");
+  const mainContainer = document.querySelector('main');
   // const user = { id: socket.id, name: "Anton", color: "blue" };
 
-  mainContainer.innerHTML = "";
+  mainContainer.innerHTML = '';
 
   createGrid(room);
   let usersInLobby = renderRoomUsers(room.users);
   mainContainer.appendChild(usersInLobby);
 
-  socket.on("gameOver", (roomWithScore) => {
+  socket.on('gameOver', (roomWithScore) => {
     if (roomWithScore.id != room.id) {
       return;
     }
-    socket.off("gameOver");
+    socket.off('gameOver');
 
     showGameOverPage(roomWithScore.score);
   });
 
   let gameTimer = 5;
-  const gameTimerHeading = document.createElement("h2");
-  gameTimerHeading.classList.add("game-timer");
+  const gameTimerHeading = document.createElement('h2');
+  gameTimerHeading.classList.add('game-timer');
   gameTimerHeading.innerHTML = gameTimer;
   mainContainer.appendChild(gameTimerHeading);
 
