d8015e49401729899f88aecfb33550ca9d394368
jenmwa <jenmwa@gmail.com>
2023-04-24 16:13:39 +0200

    new grid for style added

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,13 @@
-import './styles/style.css';
-import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
-import { viewSavedImages } from './scripts/saveImg';
-import { initLog } from './scripts/log';
-import { renderRoomsSection } from './scripts/gameRooms';
-import { renderChatHtml } from './scripts/chatcomp';
-import { renderFooter } from './scripts/footerComp';
+import "./styles/style.css";
+import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+import { viewSavedImages } from "./scripts/saveImg";
+import { initLog } from "./scripts/log";
+import { renderRoomsSection } from "./scripts/gameRooms";
+import { renderChatHtml } from "./scripts/chatcomp";
+import { renderFooter } from "./scripts/footerComp";
 
 export let grids = {};
-export const socket = io('http://localhost:3000');
+export const socket = io("http://localhost:3000");
 
 // document.querySelector('#app').innerHTML = `
 //   <div class="h-screen bg-teal-900 parent">
@@ -20,14 +20,15 @@ export const socket = io('http://localhost:3000');
 //   <footer id="footer"></footer>
 // `;
 
-document.querySelector(
-  '#app'
-).innerHTML = `<div class='h-screen bg-teal-900 parent'>
-<header class='header w-full h-full'>header</header>
-<div class='main w-full h-full'>main </div>
-<div class='chat w-full h-full'>chat </div>
-<div id='footer w-full h-full' class='footer'>footer </div>
-</div>;`;
+document.querySelector("#app").innerHTML = `
+
+<div class='h-screen bg-teal-900 parent'>
+  <header class="header w-full h-full">header</header>
+  <main class="main w-full h-full p-4 flex justify-center items-center gap-4"></main>
+  <aside class=chat w-full h-full" id="chat-div">chat </aside>
+  
+</div>
+<footer id='footer' class='footer w-full h-full'>footer </footer>`;
 
 const init = () => {
   initLog();
