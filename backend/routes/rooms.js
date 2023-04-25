4cbe7b6bd6b50864098b5f67f63095be9151d6ab
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-04-25 19:02:18 +0200

    local msg fetch and code cleanup

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const rooms = require('../modules/variables');
+const { rooms } = require("../modules/variables");
 
-router.get('/', function(req, res) {
+router.get("/", function (req, res) {
   res.json(rooms);
+});
+
+router.get("/messages/:roomId", function (req, res) {
+  const roomId = req.params.roomId;
+  const room = rooms.find((room) => room.roomId == roomId);
+
+  console.log(room.messages);
+  res.json(room.messages);
 });
 
 module.exports = router;
