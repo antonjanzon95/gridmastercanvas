5d4c7d1e5ef5cc8e95e2ec8f130b5f55c0960376
robin-sevelin <robinsevelin@hotmail.com>
2023-04-26 13:24:40 +0200

    removed function

--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,14 @@
-import "./styles/style.css";
-import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
-import { initLog } from "./scripts/log";
-import { renderChat } from "./scripts/chatcomp";
-import { renderFooter } from "./scripts/footerComp";
-import { renderStartPage } from "./scripts/startPage";
-
+import './styles/style.css';
+import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
+import { initLog } from './scripts/log';
+import { renderChat } from './scripts/chatcomp';
+import { renderFooter } from './scripts/footerComp';
+import { renderStartPage } from './scripts/startPage';
 
 export let grids = {};
-export const socket = io("http://localhost:3000");
+export const socket = io('http://localhost:3000');
 
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
 <div class='h-screen bg-teal-900 parent'>
   <header class="header w-full h-full" id="header"></header>
   <main class="main w-full h-full p-4 flex justify-center items-center gap-4"></main>
@@ -21,9 +20,7 @@ document.querySelector("#app").innerHTML = `
 const init = () => {
   initSockets();
   initLog();
-  renderStartPage() 
-  // renderHeader();
-  // renderChatHtml();
+  renderStartPage();
   renderFooter();
   // viewSavedImages();
 };
@@ -31,8 +28,8 @@ const init = () => {
 init();
 
 function initSockets() {
-  socket.on("globalMessage", (msg) => {
-    let user = JSON.parse(sessionStorage.getItem("user"));
+  socket.on('globalMessage', (msg) => {
+    let user = JSON.parse(sessionStorage.getItem('user'));
 
     const globalChat = {
       user: msg.user,
@@ -40,19 +37,19 @@ function initSockets() {
       color: msg.color,
     };
 
-    const globalMessages = JSON.parse(sessionStorage.getItem("globalMessages"));
+    const globalMessages = JSON.parse(sessionStorage.getItem('globalMessages'));
     globalMessages.unshift(globalChat);
-    sessionStorage.setItem("globalMessages", JSON.stringify(globalMessages));
+    sessionStorage.setItem('globalMessages', JSON.stringify(globalMessages));
 
     // messages.insertBefore(chat, messages.firstChild);
     // messages.scrollTop = messages.scrollHeight;
 
-    if (user.currentChat == "global") {
-      renderChat(JSON.parse(sessionStorage.getItem("globalMessages")));
+    if (user.currentChat == 'global') {
+      renderChat(JSON.parse(sessionStorage.getItem('globalMessages')));
     }
   });
 }
 
 export function removeSockets() {
-  socket.off("globalChat");
+  socket.off('globalChat');
 }
